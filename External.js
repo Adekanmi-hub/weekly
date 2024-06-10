0665443e99d54f9eaf419fcd530b774425c4a50b
Adekanmi-hub <abai.moses@gmail.com>
2024-06-10 09:36:52 +0100

    first commit

---
+++ External.js
@@ -0,0 +1,33 @@
+                // Adds two numbers
+                function addition(num1, num2)
+                {
+                    // Notice how a variable can share the same name as a function
+                    let addition = num1 + num2;
+
+                    return addition;
+                }
+
+                // Subtracts two numbers
+                function subtraction(num1, num2)
+                {
+                    let subtraction = num1 - num2;
+
+                    return subtraction;
+                }
+
+                // Multiplies two numbers
+                function multiplication(num1, num2)
+                {
+                    let subtraction = num1 * num2;
+
+                    return subtraction;
+                }
+
+
+                // Divides two numbers
+                function division(num1, num2)
+                {
+                    let division = num1 / num2;
+
+                    return division;
+                }
